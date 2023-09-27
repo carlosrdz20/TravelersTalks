6d9f6e3f2f80b38bd26e0b7785d6f4ed04340fd9
Edgar Gerardo Rodríguez Martínez <99943949+EdgarRdz777@users.noreply.github.com>
2023-09-27 02:38:26 -0600

    Creamos la primer ventana donde sera el home solamente las barras superior e inferior sin detalle el boton.

--- app/src/main/java/com/example/travelerstalks/MainActivity.kt
+++ app/src/main/java/com/example/travelerstalks/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.travelerstalks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editText = findViewById<EditText>(R.id.textoSearch)
+
+        // Create a Paint object to style the text
+        val paint = editText.paint
+
+        // Set the underline flag
+        val valor = editText.text.toString().trim()
+        paint.isUnderlineText = valor.isEmpty()
     }
 }
